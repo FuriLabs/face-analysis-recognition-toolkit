c1ed90bd50c090f77419425f43520eebb2b6ffab
Bardia Moshiri <bardia@furilabs.com>
2026-05-07 01:14:55 -0400

    treewide: bump copyright to 2026
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- test/face_auth_test.c
+++ test/face_auth_test.c
@@ -1,6 +1,6 @@
 /**
  * SPDX-License-Identifier: GPL-2.0-only
- * Copyright (C) 2025 Bardia Moshiri <bardia@furilabs.com>
+ * Copyright (C) 2026 Bardia Moshiri <bardia@furilabs.com>
  */
 
 #include <gtk/gtk.h>
